efe474a84354fc05758a1cf1c4da3e24d66527d0
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-05-11 18:11:47 -0400

    added the calculator functionality at a very basic level

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 const moneyLeft = document.querySelector(".moneyRemaining");
 document.querySelector("#submit").addEventListener("click", calculate);
 
-function calculate(e) {
-  let income = document.querySelector("#income").value;
-  let water = document.querySelectorAll("#water").value;
-  let electric = document.querySelectorAll("#electric").value;
-  let housing = document.querySelectorAll("#housing").value;
-  moneyLeft.innerText = income - (water + electric + housing);
-  e.preverntDefault();
+function calculate() {
+  var income = document.querySelector("#income").value;
+  var water = document.querySelector("#water").value;
+  var electric = document.querySelector("#electric").value;
+  var housing = document.querySelector("#housing").value;
+  moneyLeft.innerText = +income - (+water + +electric + +housing);
 }
